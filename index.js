2eae9aa74c00cebad85c6cdfeb496bb82d668855
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-06 10:46:20 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "N/A"},
+    { year: "2004", result: "N/A"},
+    { year: "2003", result: "N/A"},
+    { year: "2002", result: "N/A"},
+    { year: "2001", result: "W"},
+    { year: "2000", result: "N/A"},
+    { year: "1999", result: "N/A"},
+    { year: "1998", result: "W"},
+    { year: "1997", result: "W"},
+]
+
+// function findYear(element) {
+//     if (element.result === "W") {
+//         //console.log(element.year)
+//         return element.year
+//     }
+// }
+
+function superbowlWin(array) {
+    const firstWinYear = array.find(element => element.result === 'W') 
+    if (firstWinYear) {return firstWinYear.year
+    } else {return undefined}
+}
+
+console.log(superbowlWin(record))
